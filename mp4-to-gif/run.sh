6b8fef2ff92d1a0fc93bc7f13052dce250ceb71f
mt <mt@mts-MacBook-Air.local>
2018-05-28 01:09:57 -0400

    add scripts

---
+++ mp4-to-gif/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for filename in ./clips/*.mp4; do
+	./gif.sh $filename ./gifs/$(basename ${filename%.*}.gif)
+	#./gif.sh $filename /Users/cell/Desktop/$(basename ${filename%.*}.gif)
+	#echo $(basename $filename)
+done
