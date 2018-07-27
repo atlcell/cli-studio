0b8560d8281e7dea21ba340696b43c37ea2b03bd
mt <mt@mts-MacBook-Air.local>
2018-07-26 20:05:34 -0400

    bash format

--- clipper/clipper.sh
+++ clipper/clipper.sh
@@ -1,2 +1,4 @@
 #Clip MP$
-ffmpeg -i "/path/input.mp4" -codec copy -c:v libx264 -pix_fmt yuv420p  -ss 00:01:55 -t 00:00:03 -strict -2 /path/output.mp4
+#ffmpeg -i "/path/input.mp4" -codec copy -c:v libx264 -pix_fmt yuv420p  -ss 00:01:55 -t 00:00:03 -strict -2 /path/output.mp4
+
+ffmpeg -i $1 -codec copy -c:v libx264 -pix_fmt yuv420p  -ss $2 -t 00:00:$3 -strict -2 OUTPUT.mp4
