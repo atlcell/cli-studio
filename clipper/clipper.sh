8ecee407abe234c00deec6a19e07cbde6657b145
mt <mt@mts-MacBook-Air.local>
2018-07-28 17:30:25 -0400

    more

--- clipper/clipper.sh
+++ clipper/clipper.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+# Basic if statement
+
 #Clip MP$
 #ffmpeg -i "/path/input.mp4" -codec copy -c:v libx264 -pix_fmt yuv420p  -ss 00:01:55 -t 00:00:03 -strict -2 /path/output.mp4
 
-ffmpeg -i $1 -codec copy -c:v libx264 -pix_fmt yuv420p  -ss $2 -t 00:00:$3 -strict -2 OUTPUT.mp4
+ffmpeg -i $1 -codec copy -c:v libx264 -pix_fmt yuv420p  -ss $2 -t 00:00:$3 -strict -2 $4
--- giffer/run.sh
+++ giffer/run.sh
@@ -5,3 +5,5 @@ for filename in ./clips/*.mp4; do
 	#./gif.sh $filename /Users/cell/Desktop/$(basename ${filename%.*}.gif)
 	#echo $(basename $filename)
 done
+
+mv ./clips/* ./junkyard
