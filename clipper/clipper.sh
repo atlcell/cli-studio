c9530b04d2ac029c6f5e4e2a7068acdcc366cf2e
Cell <marcell@tisdol.com>
2018-07-29 01:46:51 -0400

    some changes

--- clipper/clipper.sh
+++ clipper/clipper.sh
@@ -4,4 +4,5 @@
 #Clip MP$
 #ffmpeg -i "/path/input.mp4" -codec copy -c:v libx264 -pix_fmt yuv420p  -ss 00:01:55 -t 00:00:03 -strict -2 /path/output.mp4
 
-ffmpeg -i $1 -codec copy -c:v libx264 -pix_fmt yuv420p  -ss $2 -t 00:00:$3 -strict -2 $4
+#ffmpeg -i "$1" -c:v libx265 -c:a copy -pix_fmt yuv420p -ss $2 -t 00:00:$3 -strict -2 -flags +global_header "$4"
+ffmpeg -i "$1" -codec copy -c:v libx264 -pix_fmt yuv420p  -ss "$2" -t 00:00:$3 -strict -2 $4 $5
