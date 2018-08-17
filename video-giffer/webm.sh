b54d6a9a2a92d138edc5643511e5f37758fd52a8
Marcell <cell@Marcells-MacBook-Air.local>
2018-08-16 23:59:52 -0400

    giffer

--- video-giffer/webm.sh
+++ video-giffer/webm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for filename in ./clips/*.mp4; do
-	ffmpeg -i $filename -pix_fmt yuv420p ./webm/$(basename ${filename%.*}.webm)
+	ffmpeg -i $filename -vcodec libvpx -qmin 0 -qmax 50 -crf 10 -b:v 1M -acodec libvorbis ./gifs/$(basename ${filename%.*}.webm)
 done
 
 mv ./clips/* ./junkyard/src
