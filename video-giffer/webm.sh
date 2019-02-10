404ad5d070f19fa482924ea95b3ebae455612359
Cell <marcell@tisdol.com>
2019-02-10 14:44:25 -0500

    multi-core rendering

--- video-giffer/webm.sh
+++ video-giffer/webm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for filename in ./input/*.mp4; do
-	ffmpeg -i $filename -vcodec libvpx -qmin 0 -qmax 50 -crf 10 -b:v 1M -acodec libvorbis ./output/$(basename ${filename%.*}.webm)
+	ffmpeg -i $filename -vcodec libvpx -threads 4 -qmin 0 -qmax 50 -crf 10 -b:v 1M -acodec libvorbis ./output/$(basename ${filename%.*}.webm)
 done
 
 mv ./input/* ./junkyard/src
