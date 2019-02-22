0e47180f96a9a1a4ff21bad74d280431d6f4374d
Cell <marcell@tisdol.com>
2019-02-21 20:58:08 -0500

    Update webm.sh

--- video-giffer/webm.sh
+++ video-giffer/webm.sh
@@ -4,4 +4,4 @@ for filename in ./input/*.mp4; do
 	ffmpeg -i $filename -vcodec libvpx -threads 4 -qmin 0 -qmax 50 -crf 10 -b:v 1M -acodec libvorbis ./output/$(basename ${filename%.*}.webm)
 done
 
-mv ./input/* ./junkyard/src
+mv ./input/* ./junkyard/
